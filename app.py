de472712d6a30674cabab5c7e8d86b69331161bd
sudenurdurkan <sudenurdurkan46@gmail.com>
2026-05-24 18:39:07 +0300

    fix: enable production redis ssl using from_url

--- app.py
+++ app.py
@@ -18,12 +18,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Redis Settings
-redis_client = redis.Redis(
-    host=os.getenv('REDIS_HOST', 'localhost'),
-    port=int(os.getenv('REDIS_PORT', 6379)),
-    password = os.getenv('REDIS_PASSWORD', None),
-    decode_responses=True
-)
+redis_url = os.getenv('REDIS_URL')
+
+if redis_url:
+    redis_client = redis.Redis.from_url(redis_url, decode_responses=True)
+else:
+    redis_client = redis.Redis(
+        host=os.getenv('REDIS_HOST', 'localhost'),
+        port=int(os.getenv('REDIS_PORT', 6379)),
+        password = os.getenv('REDIS_PASSWORD', None),
+        decode_responses=True
+    )
 
 # Prometheus Metrics
 HTTP_REQUESTS_TOTAL = Counter('http_requests_total', 'Total HTTP Requests', ['method', 'endpoint'])
